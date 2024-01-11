395476263bf6276c907d9154787437dd92c2d7b4
silin001 <862956812@qq.com>
2024-01-11 23:36:37 +0800

    添加入口文件

---
+++ src/index.js
@@ -0,0 +1,14 @@
+/*
+ * @Date: 2024-01-11 23:26:10
+ * @LastEditTime: 2024-01-11 23:26:11
+ * @Description: 入口文件
+ * @FilePath: /my-v3ts-project/Users/sisi/Desktop/myWeb/my-project/mini-vue3/src/index.js
+ */
+
+
+function Vue () {
+
+}
+
+// 导出 Vue 函数，提供外部使用
+export default Vue;
