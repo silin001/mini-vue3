748672cb1dc142e5fadba4ec80415144a906f5d4
sisi <862956812@qq.com>
2024-01-16 15:40:12 +0800

    添加基础的handler包含createGetter、createSetter

--- packages/reactivity/src/baseHandlers.js
+++ packages/reactivity/src/baseHandlers.js
@@ -1,10 +1,10 @@
 /*
  * @Date: 2024-01-15 15:47:25
- * @LastEditTime: 2024-01-15 16:33:31
+ * @LastEditTime: 2024-01-16 11:11:21
  * @Description:
  * @FilePath: \yike-design-devd:\web_si\my_webDemo\my-open-source\mini-vue3\packages\reactivity\src\baseHandlers.js
  */
-
+import { track, trigger } from './effect'
 
 const get = createGetter()
 const set = createSetter()
@@ -16,11 +16,11 @@ export const mutableHandlers = {
 
 function createGetter () {
  return function get (target, key, receiver) {
-  console.log('get读取')
+  console.log('proxy get读取')
   // 正常访问，使用 Reflect 反射对象
   const result = Reflect.get(target,key,receiver)
   // TODO 核心：依赖收集
-  // track()
+  track(target, key)
   return result
  }
 }
@@ -28,10 +28,10 @@ function createGetter () {
 
 function createSetter () {
  return function set (target, key, newValue, receiver) {
-  console.log('set设置')
+  console.log('proxy set设置')
   const result = Reflect.set(target, key, newValue, receiver)
   // TODO 触发set时候进行 依赖触发
-  // trigger
+  trigger(target, key)
   return result
  }
 }
