05a8b1e285392a561815f4c9b58cdeb43c8751ed
sisi <862956812@qq.com>
2024-01-20 12:39:58 +0800

    使用栈结构解决effect嵌套使用问题

--- packages/reactivity/src/baseHandlers.js
+++ packages/reactivity/src/baseHandlers.js
@@ -1,6 +1,6 @@
 /*
  * @Date: 2024-01-15 15:47:25
- * @LastEditTime: 2024-01-16 15:48:25
+ * @LastEditTime: 2024-01-20 12:11:03
  * @Description:
  * @FilePath: \yike-design-devd:\web_si\my_webDemo\my-open-source\mini-vue3\packages\reactivity\src\baseHandlers.js
  */
@@ -17,7 +17,7 @@ export const mutableHandlers = {
 
 function createGetter () {
  return function get (target, key, receiver) {
-  console.log('proxy get读取')
+  // console.log('proxy get读取')
   // 正常访问，使用 Reflect 反射对象
   const result = Reflect.get(target,key,receiver)
   // TODO 核心：依赖收集
@@ -34,7 +34,7 @@ function createGetter () {
 
 function createSetter () {
  return function set (target, key, newValue, receiver) {
-  console.log('proxy set设置')
+  // console.log('proxy set设置')
   const result = Reflect.set(target, key, newValue, receiver)
   // TODO 触发set时候进行 依赖触发
   trigger(target, key)
--- packages/reactivity/src/effect.js
+++ packages/reactivity/src/effect.js
@@ -1,6 +1,6 @@
 /*
  * @Date: 2024-01-16 10:36:04
- * @LastEditTime: 2024-01-16 16:33:10
+ * @LastEditTime: 2024-01-20 12:27:07
  * @Description: effect、track、trigger
  * @FilePath: \yike-design-devd:\web_si\my_webDemo\my-open-source\mini-vue3\packages\reactivity\src\effect.js
  */
@@ -8,7 +8,8 @@
 
 /** 当前被执行的effect */
 export let activeEffect
-
+/** 栈结构存储当前选中的effect */
+const effectStack = []
 
 /** 目标 */
 const targetMap = new WeakMap()
@@ -21,28 +22,28 @@ const targetMap = new WeakMap()
  * @param {*} key
  * @return {*}
  */
-export const track =  (target, key) => {
- console.log('track 依赖收集')
- // 如果当前依赖存储的effect不存在，直接退出
- if (!activeEffect) {
-  return
- }
- // 1------如果activeEffect存在，进行获取目标
- let depsMap = targetMap.get(target)
- // 如果目标不存在， 定义一个新的，并存储到targetMap
- if (!depsMap) {
-  depsMap = new Map()
-  targetMap.set(target, depsMap)
- }
- // 2-----
- let deps = depsMap.get(key)
- if (!deps) {
-  deps = new Set()
-  depsMap.set(key,deps)
- }
- // 存储
- deps.add(activeEffect)
- console.log('track 收集 deps---', deps)
+export const track = (target, key) => {
+  // console.log('track 依赖收集')
+  // 如果当前依赖存储的effect不存在，直接退出
+  if (!activeEffect) {
+    return
+  }
+  // 1------如果activeEffect存在，进行获取目标
+  let depsMap = targetMap.get(target)
+  // 如果目标不存在， 定义一个新的，并存储到targetMap
+  if (!depsMap) {
+    depsMap = new Map()
+    targetMap.set(target, depsMap)
+  }
+  // 2-----
+  let deps = depsMap.get(key)
+  if (!deps) {
+    deps = new Set()
+    depsMap.set(key, deps)
+  }
+  // 存储
+  deps.add(activeEffect)
+  //  console.log('track 收集 deps---', deps)
 }
 
 
@@ -53,19 +54,19 @@ export const track =  (target, key) => {
  * @return {*}
  */
 export const trigger = (target, key) => {
- console.log('trigger 依赖触发')
- const depsMap = targetMap.get(target)
-   // 如果拿不到Map,这说明当前对象没有effect要触发
- if (!depsMap) {
-  return
- }
- const deps = depsMap.get(key)
- // 虽然当前对象中存在effect, 但是本次读取的 对象内变量 不存在,所以此处获取不到值
- if (!deps) {
-  return
- }
- // 触发所有依赖
- triggerEffects(deps)
+  // console.log('trigger 依赖触发')
+  const depsMap = targetMap.get(target)
+  // 如果拿不到Map,这说明当前对象没有effect要触发
+  if (!depsMap) {
+    return
+  }
+  const deps = depsMap.get(key)
+  // 虽然当前对象中存在effect, 但是本次读取的 对象内变量 不存在,所以此处获取不到值
+  if (!deps) {
+    return
+  }
+  // 触发所有依赖
+  triggerEffects(deps)
 
 }
 
@@ -78,22 +79,33 @@ export const trigger = (target, key) => {
  * @return {*}
  */
 export const effect = (fn) => {
- const _effect = new ReactiveEffect(fn)
- // 完成第一次执行
- _effect.run()
+  const _effectFn = new ReactiveEffect(fn)
+  // 初始化（第一次）执行effect传入的fn
+  // console.log('effect完成第一次初始化')
+  _effectFn.run()
+  return _effectFn
 }
 
 /** 依赖收集 */
-class ReactiveEffect{
- constructor(fn) {
-  this.fn = fn
-}
- run () {
-  console.log('run....')
-  // 执行的时候给全局的 activeEffect 赋值, 利用全局属性来获取当前的 effect
-  activeEffect = this
-  // 执行用户传入的fn
-  return this.fn()
+class ReactiveEffect {
+  constructor(fn) {
+    this.fn = fn
+  }
+  run () {
+    // console.log('run... this——>', this)
+    try {
+      // 使用栈存储一下，压入栈
+      effectStack.push(this)
+       // 直接给全局的 activeEffect 赋值，利用全局属性来获取当前的 effect
+      activeEffect = this // 虽然上面使用了栈存储，但这里还要必须赋值一次，因为要return
+      // 执行用户传入的fn，并返回
+      return this.fn()
+    } finally {
+      // 出栈
+      effectStack.pop()
+      // 获取当前effect
+      activeEffect = effectStack[effectStack.length-1]
+    }
   }
 }
 
@@ -104,8 +116,8 @@ class ReactiveEffect{
  * @return {*}
  */
 export const triggerEffects = (deps) => {
- console.log('收集到的所有effect：', deps)
- for (const effect of deps) {
-   effect.run()
+  // console.log('收集到的所有effect：', deps)
+  for (const effect of deps) {
+    effect.run()
   }
 }
